7bc333827cd72409bdb46014c385f3d02f049ef1
ivanlysogor <ilysogor@gmail.com>
2017-04-16 02:16:20 +0300

    223

--- mqtt-test.py
+++ mqtt-test.py
@@ -31,7 +31,7 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
     r.append(msg.topic,str(msg.payload)+'/')
-    pubnub.publish().channel("TEST").message("TEST").async(publish_callback)
+    pubnub.publish().channel("TEST").message({"text":"Enter Message Heresss"}).async(publish_callback)
     pubnub.publish().channel(msg.topic).message(str(msg.payload)).async(publish_callback)
 
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
