3740c6a3777aa9b88e9d5067f0e0538980986901
ivanlysogor <ilysogor@gmail.com>
2017-04-16 01:51:14 +0300

    dd

--- mqtt-test.py
+++ mqtt-test.py
@@ -2,15 +2,19 @@ import paho.mqtt.client as mqtt
 import json
 import redis
 
+from pubnub.pnconfiguration import PNConfiguration
+from pubnub.pubnub import PubNub
+ 
+pnconfig = PNConfiguration()
+pnconfig.subscribe_key = "sub-c-a2885d3a-222d-11e7-b284-02ee2ddab7fe"
+pnconfig.publish_key = "pub-c-aa6a2b58-13a0-4b05-abce-20cf4cc4f251"
+pnconfig.ssl = False
+ 
+pubnub = PubNub(pnconfig)
 
-publish_key   = "pub-c-2272ef16-05d2-4cfb-927a-7a7c4232145b"
-subscribe_key = "sub-c-b4c7721e-2229-11e7-bd07-02ee2ddab7fe"
-channel_name  = "devices/lora/807B85902000017A/lmt01"
-client_uuid   = "1ddb86ef5"
-
-mqtt_hostname = "mqtt.pubnub.com"
-mqtt_connect  = publish_key + "/" + subscribe_key + "/" + client_uuid
-mqtt_topic    = publish_key + "/" + subscribe_key + "/" + channel_name
+def publish_callback(result, status):
+    pass
+    # Handle PNPublishResult and PNStatus
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -25,7 +29,7 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
     r.append(msg.topic,str(msg.payload)+'/')
-    mosq_object.publish( mqtt_topic, msg.payload)
+    pubnub.publish().channel(msg.topic).message(str(msg.payload)).async(publish_callback)
 
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
@@ -34,9 +38,6 @@ client.on_connect = on_connect
 client.on_message = on_message
 
 client.connect("localhost", 1883, 60)
-
-mosq_object = mqtt.Client()
-mosq_object.connect(mqtt_hostname)
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
